c7d4b6acefcef11f8f3970a79a5ca552901bb8ae
= <2709400@gmail.com>
2024-09-25 08:51:39 +0300

    last commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,406 @@
+var rgb = { r: 255, g: 255, b: 255 };
+var xyz = rgb_to_xyz(rgb);
+var lab = rgb_to_lab(rgb);
+  
+var rounding_data = false;
+var data_rounded = document.getElementById('data-rounded');
+  
+var first_input = document.getElementById('first_input');
+var second_input = document.getElementById('second_input');
+var third_input = document.getElementById('third_input');
+  
+first_input.addEventListener('input', on_input);
+second_input.addEventListener('input', on_input);
+third_input.addEventListener('input', on_input);
+  
+var first_hint = document.getElementById('first_hint');
+var second_hint = document.getElementById('second_hint');
+var third_hint = document.getElementById('third_hint');
+  
+var first_input_wrong = false;
+var second_input_wrong = false;
+var third_input_wrong = false;
+  
+var first_input_title = document.getElementById('first_input__title');
+var second_input_title = document.getElementById('second_input__title');
+var third_input_title = document.getElementById('third_input__title');
+  
+var output_rgb = document.getElementById('rgb-output__data');
+var output_xyz = document.getElementById('xyz-output__data');
+var output_lab = document.getElementById('lab-output__data');
+  
+output_rgb.addEventListener('DOMSubtreeModified', set_rgb_color);
+var output_color_rgb = document.getElementById('color-output__rgb');
+  
+var color_picker = new iro.ColorPicker('#picker');
+  
+color_picker.on('input:change', function (color) {
+    rgb = color.rgb;
+    xyz = rgb_to_xyz(rgb);
+    lab = rgb_to_lab(rgb);
+    update_fields_by_colorpicker();
+});
+  
+var drop_down = document.getElementById('drop-down');
+var selected_color_type = drop_down.value;
+  
+drop_down.addEventListener("change", function () {
+    selected_color_type = drop_down.value;
+    change_input_titles();
+    change_access_fourth_input();
+    change_input_data();
+    on_input();
+});
+  
+var swatch_grid = document.getElementById('swatch-grid');
+  
+swatch_grid.addEventListener('click', function (e) {
+    var click_target = e.target;
+    if (click_target.dataset.color) {
+        color_picker.color.set(click_target.dataset.color);
+        rgb = color_picker.color.rgb;
+        xyz = rgb_to_xyz(rgb);
+        lab = rgb_to_lab(rgb);
+        update_fields_by_colorpicker();
+    }
+});
+  
+init_data();
+  
+/* ========== Initialize fields ========== */
+
+function init_data() {
+    first_input_title.textContent = 'R:';
+    second_input_title.textContent = 'G:';
+    third_input_title.textContent = 'B:';
+  
+    set_inputs([Math.round(rgb.r), Math.round(rgb.g), Math.round(rgb.b)]);
+    set_rgb_fields(rgb);
+    set_xyz_fields(xyz);
+    set_lab_fields(lab);
+}
+  
+/* ========== Update fields ========== */
+
+function update_fields_by_colorpicker() {
+    rounding_data = false;
+    set_rgb_fields(rgb);
+    set_xyz_fields(xyz);
+    set_lab_fields(lab);
+    switch (selected_color_type) {
+        case 'rgb':
+            set_inputs([Math.round(rgb.r), Math.round(rgb.g), Math.round(rgb.b)]);
+            break;
+        case 'xyz':
+            set_inputs([xyz.x.toFixed(2), Math.round(xyz.y), xyz.z.toFixed(2)]);
+            break;
+        case 'lab':
+            set_inputs([Math.round(lab.l), Math.round(lab.a), Math.round(lab.b)]);
+            break;
+    }
+    is_valid_input();
+    set_hints();
+    hide_hints();
+}
+  
+function on_input() {
+    if (!is_valid_input()) {
+        set_undefined();
+        set_hints();
+        hide_hints();
+  
+    } else {
+        switch (selected_color_type) {
+            case 'rgb': {
+                rgb = get_data_from_inputs();
+                xyz = rgb_to_xyz(rgb);
+                lab = rgb_to_lab(rgb);
+                break;
+            }
+            case 'xyz': {
+                xyz = get_data_from_inputs();
+                rgb = xyz_to_rgb(xyz);
+                lab = rgb_to_lab(rgb);
+                break;
+            }
+            case 'lab': {
+                lab = get_data_from_inputs();
+                rgb = lab_to_rgb(lab);
+                xyz = rgb_to_xyz(rgb);
+                break;
+            }
+        }
+        set_rgb_fields(rgb);
+        set_xyz_fields(xyz);
+        set_lab_fields(lab);
+        color_picker.color.rgb = rgb;
+        hide_hints();
+    }
+}
+  
+function get_data_from_inputs() {
+    switch (selected_color_type) {
+        case 'rgb':
+            return { r: first_input.value, g: second_input.value, b: third_input.value };
+        case 'xyz':
+            return { x: first_input.value, y: second_input.value, z: third_input.value };
+        case 'lab':
+            return { l: first_input.value, a: second_input.value, b: third_input.value };
+    }
+}
+  
+function set_rgb_color() {
+    output_color_rgb.style.backgroundColor = rgb.r + ',' + rgb.g + ',' + rgb.b;
+}
+  
+/* ========== Check input values ========== */
+
+function is_valid_rgb() {
+    let rgb = get_data_from_inputs()
+  
+    if (rgb.r < 0 || rgb.r > 255 || rgb.r == '') {
+        first_input_wrong = true;
+    } else {
+        first_input_wrong = false;
+    }
+  
+    if (rgb.g < 0 || rgb.g > 255 || rgb.g == '') {
+        second_input_wrong = true;
+    } else {
+        second_input_wrong = false;
+    }
+  
+    if (rgb.b < 0 || rgb.b > 255 || rgb.b == '') {
+        third_input_wrong = true;
+    } else {
+        third_input_wrong = false;
+    }
+  
+    fourth_input_wrong = false;
+    return !first_input_wrong && !second_input_wrong && !third_input_wrong;
+}
+  
+function is_valid_xyz() {
+    let xyz = get_data_from_inputs();
+  
+    if (xyz.x < 0 || xyz.x > 95.047 || xyz.x == '') {
+        first_input_wrong = true;
+    } else {
+        first_input_wrong = false;
+    }
+  
+    if (xyz.y < 0 || xyz.y > 100 || xyz.y == '') {
+        second_input_wrong = true;
+    } else {
+        second_input_wrong = false;
+    }
+  
+    if (xyz.z < 0 || xyz.z > 108.883 || xyz.z == '') {
+        third_input_wrong = true;
+    } else {
+        third_input_wrong = false;
+    }  
+
+    return !first_input_wrong && !second_input_wrong && !third_input_wrong;
+}
+
+function is_valid_lab() {
+    let lab = get_data_from_inputs();
+  
+    if (lab.l > 100 || lab.l < 0 || lab.l == '') {
+        first_input_wrong = true;
+    } else {
+        first_input_wrong = false;
+    }
+  
+    if (lab.a > 127 || lab.a < -128 || lab.a == '') {
+        second_input_wrong = true;
+    } else {
+        second_input_wrong = false;
+    }
+  
+    if (lab.b > 127 || lab.b < -128 || lab.b == '') {
+        third_input_wrong = true;
+    } else {
+        third_input_wrong = false;
+    }
+  
+    fourth_input_wrong = false
+    return !first_input_wrong && !second_input_wrong && !third_input_wrong;
+}
+  
+function is_valid_input() {
+    switch (selected_color_type) {
+        case 'rgb':
+            return is_valid_rgb();
+        case 'xyz':
+            return is_valid_xyz();
+        case 'lab':
+            return is_valid_lab();
+    }
+}
+  
+function set_undefined() {
+    let undefined = 'не определено';
+    output_rgb.textContent = undefined;
+    output_xyz.textContent = undefined;
+    output_lab.textContent = undefined;
+    output_rgb.style.color = "#e85a4f";
+    output_xyz.style.color = "#e85a4f";
+    output_lab.style.color = "#e85a4f";
+}
+  
+function set_hints() {
+    switch (selected_color_type) {
+        case 'rgb':
+            set_rgb_hints();
+            break;
+        case 'xyz':
+            set_xyz_hints();
+            break;
+        case 'lab':
+            set_lab_hints();
+            break;
+    }
+}
+  
+function set_rgb_hints() {
+    if (first_input_wrong) {
+        first_hint.textContent = 'от 0 до 255';
+        first_hint.style.visibility = "visible";
+        first_hint.style.color = "#e85a4f";
+    }
+    if (second_input_wrong) {
+        second_hint.textContent = 'от 0 до 255';
+        second_hint.style.visibility = "visible";
+        second_hint.style.color = "#e85a4f";
+    }
+    if (third_input_wrong) {
+        third_hint.textContent = 'от 0 до 255';
+        third_hint.style.visibility = "visible";
+        third_hint.style.color = "#e85a4f";
+    }
+}
+  
+function set_xyz_hints() {
+    if (first_input_wrong) {
+        first_hint.textContent = 'от 0 до 95.047';
+        first_hint.style.visibility = "visible";
+        first_hint.style.color = "#e85a4f";
+    }
+    if (second_input_wrong) {
+        second_hint.textContent = 'от 0 до 100';
+        second_hint.style.visibility = "visible";
+        second_hint.style.color = "#e85a4f";
+    }
+    if (third_input_wrong) {
+        third_hint.textContent = 'от 0 до 108.883';
+        third_hint.style.visibility = "visible";
+        third_hint.style.color = "#e85a4f";
+    }
+}
+  
+function set_lab_hints() {
+    if (first_input_wrong) {
+        first_hint.textContent = 'от 0 до 100';
+        first_hint.style.visibility = "visible";
+        first_hint.style.color = "#e85a4f";
+    }
+    if (second_input_wrong) {
+        second_hint.textContent = 'от -128 до 127';
+        second_hint.style.visibility = "visible";
+        second_hint.style.color = "#e85a4f";
+    }
+    if (third_input_wrong) {
+        third_hint.textContent = 'от -128 до 127';
+        third_hint.style.visibility = "visible";
+        third_hint.style.color = "#e85a4f";
+    }
+}
+  
+function hide_hints() {
+    if (!first_input_wrong) {
+        first_hint.style.visibility = "hidden";
+    }
+    if (!second_input_wrong) {
+        second_hint.style.visibility = "hidden";
+    }
+    if (!third_input_wrong) {
+        third_hint.style.visibility = "hidden";
+    }
+}
+
+/* ========== Functions to set all converted data to fields ========== */
+
+function set_inputs(values) {
+    first_input.value = values[0];
+    second_input.value = values[1];
+    third_input.value = values[2];
+}
+  
+function set_rgb_fields(rgb) {
+    output_rgb.style.color = "#17252a";
+    output_rgb.textContent = Math.round(rgb.r) + ', ' + Math.round(rgb.g) + ', ' + Math.round(rgb.b);
+  
+    if (selected_color_type == 'lab' && rounding_data) {
+        data_rounded.style.visibility = "visible";
+    } else {
+        data_rounded.style.visibility = "hidden";
+    }
+}
+  
+function set_xyz_fields(xyz) {
+    output_xyz.style.color = "#17252a";
+    output_xyz.textContent = Number(xyz.x).toFixed(2) + ', ' + Math.round(xyz.y) + ', ' + Number(xyz.z).toFixed(2);
+}
+  
+function set_lab_fields(lab) {
+    output_lab.style.color = "#17252a";
+    output_lab.textContent = Math.round(lab.l) + ', ' + Math.round(lab.a) + ', ' + Math.round(lab.b);
+}
+
+/* ========== Functions to change inputs on drop-down change ========== */
+function change_input_data() {
+    switch (selected_color_type) {
+        case 'rgb':
+            first_input.value = Math.round(rgb.r);
+            second_input.value = Math.round(rgb.g);
+            third_input.value = Math.round(rgb.b);
+            break;
+        case 'xyz':
+            first_input.value = xyz.x.toFixed(2);
+            second_input.value = Math.round(xyz.y);
+            third_input.value = xyz.z.toFixed(2);
+            break;
+        case 'lab':
+            first_input.value = Math.round(lab.l);
+            second_input.value = Math.round(lab.a);
+            third_input.value = Math.round(lab.b);
+            break;
+    }
+}
+
+function change_input_titles() {
+    switch (selected_color_type) {
+        case 'rgb':
+            first_input_title.textContent = 'R:';
+            second_input_title.textContent = 'G:';
+            third_input_title.textContent = 'B:';
+            break;
+        case 'xyz':
+            first_input_title.textContent = 'X:';
+            second_input_title.textContent = 'Y:';
+            third_input_title.textContent = 'Z:';
+            break;
+        case 'lab':
+            first_input_title.textContent = 'L:';
+            second_input_title.textContent = 'A:';
+            third_input_title.textContent = 'B:';
+            break;
+    }
+}
+
+/* ========== Color conversion functions ========== */
 
 function rgb_to_cmyk(rgb) {
 	let k = Math.min(1 - Number(rgb.r) / 255, 1 - Number(rgb.g) / 255, 1 - Number(rgb.b) / 255);
